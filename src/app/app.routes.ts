f6e5873568eb4474ab7fed620be0ec9e01813889
Prashant Singh <prashant02ps@gmail.com>
2023-12-01 16:02:57 +0530

    about page integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DetailsPokemonComponent } from './components/details-pokemon/details-pokemon.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
 	{path: 'home', redirectTo: '/dashboard'},
@@ -10,7 +11,7 @@ export const routes: Routes = [
 		path: 'pokemon/:nameID', 
 		component: DetailsPokemonComponent
 	},
-	// {path: 'home', component: HomeComponent},
+	{path: 'about', component: AboutComponent},
 	
 	{ path: '**', component: NotFoundComponent }
 ];
